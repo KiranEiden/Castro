b7eaad1676d0d969f73d16063837c0d089bfaf4e
Kiran Eiden <kiran.eiden@stonybrook.edu>
2019-07-10 13:28:46 -0400

    Working spherical cloud setup

--- Exec/radiation_tests/SphericalCloud/Prob_nd.F90
+++ Exec/radiation_tests/SphericalCloud/Prob_nd.F90
@@ -3,8 +3,6 @@ subroutine amrex_probinit(init, name, namlen, problo, probhi) bind(C, name="amre
   use probdata_module
   use network, only: nspec
   use amrex_error_module
-  use eos_module
-  use eos_type_module, only: eos_t, eos_input_rt
   use amrex_fort_module, only: rt => amrex_real
   implicit none
   
@@ -12,7 +10,6 @@ subroutine amrex_probinit(init, name, namlen, problo, probhi) bind(C, name="amre
   integer, intent(in) :: name(namlen)
   double precision, intent(in) :: problo(:), probhi(:)
   
-  type(eos_t) :: eos_state
   integer :: untin, i
   
   namelist /fortin/ r_cloud, rho_cloud, T_0, v_max, alpha
@@ -30,6 +27,13 @@ subroutine amrex_probinit(init, name, namlen, problo, probhi) bind(C, name="amre
      probin(i:i) = char(name(i))
   end do
   
+  ! allocate reals
+  allocate(rho_cloud)
+  allocate(r_cloud)
+  allocate(T_0)
+  allocate(v_max)
+  allocate(alpha)
+  
   ! set defaults
   r_cloud = 3.086d17
   rho_cloud = 1.6156913825823182d-18
@@ -42,24 +46,6 @@ subroutine amrex_probinit(init, name, namlen, problo, probhi) bind(C, name="amre
   open(untin,file=probin(1:namlen),form='formatted',status='old')
   read(untin,fortin)
   close(unit=untin)
-  
-  eos_state % rho = rho_cloud
-  eos_state % T   = T_0
-  eos_state % xn  = 0.e0_rt
-  eos_state % xn(1) = 1.e0_rt
-  
-  call eos(eos_input_rt, eos_state)
-
-  rhoe_cloud = rho_cloud * eos_state % e
-
-  eos_state % rho = alpha * rho_cloud
-  eos_state % T   = alpha * T_0
-  eos_state % xn  = 0.e0_rt
-  eos_state % xn(1) = 1.e0_rt
-
-  call eos(eos_input_rt, eos_state)
-
-  rhoe_ext = alpha * rho_cloud * eos_state % e
 
 end subroutine amrex_probinit
 
@@ -84,9 +70,7 @@ end subroutine amrex_probinit
 ! :::              right hand corner of grid.  (does not include
 ! :::		   ghost region).
 ! ::: -----------------------------------------------------------
-subroutine ca_initdata(level, time, lo, hi, nscal, &
-                       state, state_lo, state_hi, &
-                       delta, plo, phi)
+subroutine ca_initdata(lo, hi, state, s_lo, s_hi, dx, problo) bind(C, name='ca_initdata')
 
   use probdata_module
   use meth_params_module, only: NVAR, URHO, UMX, UMY, UMZ, UEDEN, UEINT, UFS, UFX, UTEMP
@@ -96,44 +80,37 @@ subroutine ca_initdata(level, time, lo, hi, nscal, &
 
   implicit none
 
-  integer, intent(in) :: level, nscal
-  integer, intent(in) :: lo(3), hi(3)
-  integer, intent(in) :: state_lo(3), state_hi(3)
-  real(rt), intent(in) :: plo(3), phi(3), time, delta(3)
-  real(rt), intent(inout) :: state(state_lo(1):state_hi(1), &
-                                   state_lo(2):state_hi(2), &
-                                   state_lo(3):state_hi(3), nscal)
+  integer,  intent(in) :: lo(3), hi(3)
+  integer,  intent(in) :: s_lo(3), s_hi(3)
+  real(rt), intent(inout) :: state(s_lo(1):s_hi(1), s_lo(2):s_hi(2), s_lo(3):s_hi(3), NVAR)
+  real(rt), intent(in) :: dx(3), problo(3)
 
-  real(rt) :: x, y, z, r, fac, rhoe
+  real(rt) :: x, y, z, r, fac
   integer :: i, j, k
 
   do k = lo(3), hi(3)
       
-    z = plo(3) + delta(3)*(float(k) + 0.5e0_rt)
+    z = problo(3) + dx(3) * dble(k)
     
     do j = lo(2), hi(2)
         
-        y = plo(2) + delta(2)*(float(j) + 0.5e0_rt)
+        y = problo(2) + dx(2) * dble(j)
         
         do i = lo(1), hi(1)
             
-           x = plo(1) + delta(1)*(float(i) + 0.5e0_rt)
+           x = problo(1) + dx(1) * dble(i)
            r = sqrt(x**2 + y**2 + z**2)
            
            if(r > r_cloud) then
               fac = alpha
-              rhoe = rhoe_ext
            else
               fac = 1.e0_rt
-              rhoe = rhoe_cloud
            end if
            
            state(i, j, k, URHO) = fac * rho_cloud
            state(i, j, k, UMX) = fac * v_max * x * rho_cloud / r_cloud
            state(i, j, k, UMY) = fac * v_max * y * rho_cloud / r_cloud
            state(i, j, k, UMZ) = fac * v_max * z * rho_cloud / r_cloud
-           state(i, j, k, UEDEN) = rhoe
-           state(i, j, k, UEINT) = rhoe
            state(i, j, k, UTEMP) = fac * T_0
 
            ! set the composition to be all in the first species
--- Exec/radiation_tests/SphericalCloud/probdata.F90
+++ Exec/radiation_tests/SphericalCloud/probdata.F90
@@ -2,8 +2,8 @@ module probdata_module
 
   ! Cloud parameters
   use amrex_fort_module, only : rt => amrex_real
-  real(rt)        , save ::  rho_cloud, r_cloud, T_0, v_max, alpha
+  real(rt), allocatable ::  rho_cloud, r_cloud, T_0, v_max, alpha
   
-  real(rt)        , save :: xmin, xmax, ymin, ymax, zmin, zmax
+  real(rt), allocatable :: xmin, xmax, ymin, ymax, zmin, zmax
       
 end module probdata_module
