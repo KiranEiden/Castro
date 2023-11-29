848ae58448692ef23d88e0f2d53ae28de00042d2
Kiran Eiden <kiraneiden@gmail.com>
2023-11-28 20:47:51 -0800

    Updates to parameter generation script

--- Exec/science/magnetar_supernova/get_params.py
+++ Exec/science/magnetar_supernova/get_params.py
@@ -16,16 +16,21 @@ parser.add_argument('-Mej', type=float, default=1.0)
 parser.add_argument('-Mcsm', type=float, default=1e-2)
 parser.add_argument('-Mdot', type=float, default=1e-5)
 parser.add_argument('-vw', type=float, default=10.0)
-parser.add_argument('-Rmax', type=float, default=5e14)
+parser.add_argument('-Rmax', type=float, default=1e14)
 parser.add_argument('-beta', type=float, default=3e-2)
 parser.add_argument('-t0', type=float, default=1e2)
 parser.add_argument('-eps', type=float, default=1e-8)
 parser.add_argument('-tmfac', type=float, default=1.0)
 parser.add_argument('-Efac', type=float, default=1.0)
-parser.add_argument('-tmax', type=float, default=1e4)
+parser.add_argument('-tmax', type=float, default=5.0)
 parser.add_argument('-fdep', type=float, default=0.05)
 parser.add_argument('-depscale', type=float, default=3.0)
-parser.add_argument('-chi', type=float, default=1e-2) 
+parser.add_argument('-chi', type=float, default=1e-2)
+parser.add_argument('-Mmag', type=float, default=1.256913)
+parser.add_argument('-mu', type=float, default=1e-2)
+parser.add_argument('-res', type=int, default=8192)
+parser.add_argument('-ni_mass', type=float, default=0.125)
+parser.add_argument('-blend_thresh', type=float, default=1e-30)
 
 args = parser.parse_args(sys.argv[1:])
 print("Arguments are:")
@@ -45,10 +50,18 @@ beta = args.beta
 t_0 = args.t0 * u.s
 t_m = args.tmfac * 2047.49866274 * u.s
 E_0 = args.Efac * 1.97392088e52 * u.erg
-t_max = args.tmax * u.s
+t_max = args.tmax * t_m
 f_dep = args.fdep
 depscale = args.depscale
 chi = args.chi
+M_mag = args.Mmag * u.Msun
+mu = args.mu
+res = args.res
+dr = R_max / res
+
+ni_mass = args.ni_mass
+o_mass = ni_mass * 3.
+blend_thresh = args.blend_thresh
 
 delt = 1.0; f_r = 0.25
 
@@ -69,18 +82,41 @@ E_sn = 0.5 * M_ej * zeta * (beta * u.c)**2
 p_dep = np.log(chi * f_dep**3 * E_sn / E_0 * (1. - 1 / (t_max/t_m + 1.))) / (1. - depscale)
 ebg = np.exp(p_dep * depscale)
 eb = np.exp(p_dep)
-e = np.exp(1.)
-frac_lum_base = (p_dep**3*ebg*eb*e) / (p_dep**3*ebg*eb*e - 3.*p_dep**2*depscale**2*eb + 3.*p_dep**2*ebg - 6.*p_dep*depscale*eb + 6.*p_dep*ebg - 6.*eb + 6.*ebg)
+frac_lum_base = p_dep**3*ebg/eb / (p_dep**3*ebg/eb - 3.*p_dep**2*depscale**2 + 3.*p_dep**2*ebg/eb
+        - 6.*p_dep*depscale + 6.*p_dep*ebg/eb + 6.*ebg/eb - 6.)
 
 alph = (6 - d) / (5 - d)
 zeta_tr = 2 * (n - 5) * (9 - 2*d) * (11 - 2*d) / ((5 - d)**2 * (n - d) * (3 - d))
 t_tr = zeta_tr * E_sn / E_0 * t_m
 t_tag = (f_dep*r_0*depscale * t_tr**(alph - 1) / (beta * u.c))**(1/alph)
-    
+
+M_inj = (E_0 * t_max / t_m / (t_max/t_m + 1.) / u.c**2).to(u.g)
+v_esc = ((4.0*u.G*M_mag/dr)**0.5).to(u.cm/u.s)
+E_K_inj = (0.5 * M_inj * v_esc**2).to(u.erg)
+M_mult = (mu*E_0 / E_K_inj).to(u.dimensionless)
+
+dep_cells = int((f_dep * r_0 * (depscale - 1.)) / dr + 0.5)
+
+r_t = f_r * r_0
+r = (np.arange(res) + 0.5) * dr
+r_1 = r[r < r_t]
+r_2 = r[r >= r_t]
+M_enc = np.zeros(res)
+M_enc[r < r_t] = (4.*np.pi*r_1**3 * rho0_ej * (r_1/r_t)**(-d) / (3. - d)).to(u.Msun).d
+M_enc[r >= r_t] = (4.*np.pi*r_t**3 * rho0_ej / (3. - d) + (4.*np.pi*r_2**3 * rho0_ej * (r_2/r_t)**(-n)
+        - 4.*np.pi*r_t**3 * rho0_ej) / (3. - n)).to(u.Msun).d
+M_enc = M_enc * u.Msun
+M_enc = M_enc / M_ej
+
+ni_cells = (M_enc < ni_mass).sum()
+o_cells = (M_enc < o_mass).sum() - ni_cells
+blend_scale = -ni_mass / np.log(blend_thresh)
+
+print(f"{Fore.BLUE}t_max{Style.RESET_ALL}: {Fore.GREEN}{t_max.d}{Style.RESET_ALL}")
 print(f"{Fore.BLUE}f_r{Style.RESET_ALL}: {Fore.GREEN}{f_r.d}{Style.RESET_ALL}")
 print(f"{Fore.BLUE}1/f_r{Style.RESET_ALL}: {Fore.GREEN}{1./f_r.d}{Style.RESET_ALL}")
 print(f"{Fore.BLUE}r_0{Style.RESET_ALL}: {Fore.GREEN}{r_0.to(u.cm):.15e}{Style.RESET_ALL}")
-print(f"{Fore.BLUE}r_t{Style.RESET_ALL}: {Fore.GREEN}{(r_0*f_r).to(u.cm):.15e}{Style.RESET_ALL}")
+print(f"{Fore.BLUE}r_t{Style.RESET_ALL}: {Fore.GREEN}{(r_t).to(u.cm):.15e}{Style.RESET_ALL}")
 print(f"{Fore.BLUE}rho0_ej{Style.RESET_ALL}: {Fore.GREEN}{rho0_ej.to(u.g/u.cm**3):.15e}{Style.RESET_ALL}")
 print(f"{Fore.BLUE}f_a{Style.RESET_ALL}: {Fore.GREEN}{xi.d}{Style.RESET_ALL}")
 print(f"{Fore.BLUE}rho_a{Style.RESET_ALL}: {Fore.GREEN}{rho_a.to(u.g/u.cm**3):.15e}{Style.RESET_ALL}")
@@ -89,3 +125,8 @@ print(f"{Fore.BLUE}E_sn{Style.RESET_ALL}: {Fore.GREEN}{E_sn.to(u.erg):.15e}{Styl
 print(f"{Fore.BLUE}p_dep{Style.RESET_ALL}: {Fore.GREEN}{p_dep:.15e}{Style.RESET_ALL}")
 print(f"{Fore.BLUE}frac_lum_base{Style.RESET_ALL}: {Fore.GREEN}{frac_lum_base:.15e}{Style.RESET_ALL}")
 print(f"{Fore.BLUE}t_tag{Style.RESET_ALL}: {Fore.GREEN}{t_tag:.15e}{Style.RESET_ALL}")
+print(f"{Fore.BLUE}M_mult{Style.RESET_ALL}: {Fore.GREEN}{M_mult:.15e}{Style.RESET_ALL}")
+print(f"{Fore.BLUE}dep_cells{Style.RESET_ALL}: {Fore.GREEN}{dep_cells}{Style.RESET_ALL}")
+print(f"{Fore.BLUE}ni_cells{Style.RESET_ALL}: {Fore.GREEN}{ni_cells}{Style.RESET_ALL}")
+print(f"{Fore.BLUE}o_cells{Style.RESET_ALL}: {Fore.GREEN}{o_cells}{Style.RESET_ALL}")
+print(f"{Fore.BLUE}blend_scale{Style.RESET_ALL}: {Fore.GREEN}{blend_scale:.15e}{Style.RESET_ALL}")
