1a977f3bdecd4fa778dcd58d2c55913cdfc3fd71
Kiran Eiden <kiraneiden@gmail.com>
2024-03-20 00:19:22 -0700

    Alternate breakout time estimator; argument lists
    - Alternate breakout time estimator that uses a power law fit
    - Allow different arguments for each data file

--- Exec/science/magnetar_supernova/util/shock_plotter.py
+++ Exec/science/magnetar_supernova/util/shock_plotter.py
@@ -10,13 +10,41 @@ from scipy.optimize import curve_fit
 parser = argparse.ArgumentParser()
 parser.add_argument('datafiles', nargs="*")
 parser.add_argument('-c', '--cmap')
-parser.add_argument('--breakout_thresh', type=float, default=1.1)
-parser.add_argument('--window_size', type=int, default=4)
-parser.add_argument('--breakout_frac', type=float, default=0.75)
-parser.add_argument('--tmax_xs', type=float)
-parser.add_argument('--tmax_tbo', type=float)
-parser.add_argument('--tmax_fit', type=float)
+parser.add_argument('--breakout_thresh', type=float, nargs='*', default=[1.1])
+parser.add_argument('--window_size', type=int, nargs='*', default=[4])
+parser.add_argument('--breakout_frac', type=float, nargs='*', default=[0.75])
+parser.add_argument('--tmax_xs', type=float, nargs='*')
+parser.add_argument('--tmax_tbo', type=float, nargs='*')
+parser.add_argument('--tmax_fit', type=float, nargs='*')
+parser.add_argument('--plot_int', type=int, nargs='*', default=[1])
+parser.add_argument('--breakout_summary', action='store_true')
 args = parser.parse_args()
+
+class ArgList(list):
+    
+    def __init__(self, iterable):
+        
+        if iterable is None:
+            super().__init__()
+        else:
+            super().__init__(iterable)
+                
+    def __getitem__(self, idx):
+        
+        if isinstance(idx, slice):
+            outlist = []
+            for i in range(slice.start, slice.stop, slice.step):
+                outlist.append(super().__getitem__(i % len(self)))
+            return outlist
+        return super().__getitem__(idx % len(self))
+        
+args.breakout_thresh = ArgList(args.breakout_thresh)
+args.window_size = ArgList(args.window_size)
+args.breakout_frac = ArgList(args.breakout_frac)
+args.tmax_xs = ArgList(args.tmax_xs)
+args.tmax_tbo = ArgList(args.tmax_tbo)
+args.tmax_fit = ArgList(args.tmax_fit)
+args.plot_int = ArgList(args.plot_int)
 
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
@@ -24,9 +52,13 @@ colors = prop_cycle.by_key()['color']
 if len(args.datafiles) < 1:
     sys.exit("No files were available to be loaded.")
     
-def analytical_func(ttild, A, alpha):
+def analytical_pos(ttild, A, alpha):
     
     return A * ttild**alpha
+    
+def analytical_vel(ttild, A, alpha):
+    
+    return A * alpha * ttild**(alpha - 1.)
 
 for i, fname in enumerate(args.datafiles):
     
@@ -42,30 +74,37 @@ for i, fname in enumerate(args.datafiles):
     r_outer = f["shock_pos"]["r_outer"][()]
     r_inner = f["shock_pos"]["r_inner"][()]
     
+    dr, dz = f["dx"][()]
+    dx = np.sqrt(dr**2 + dz**2)
+    rmin, zmin = f["domain_left_edge"][()]
+    rmax, zmax = f["domain_right_edge"][()]
+    
     if "teng" in f.keys():
         teng, = f["teng"][()]
     else:
         teng = 2047.49866274
     R_t = 0.1 * teng * 0.02 * 2.997e10
     dttild = 0.01
-    drtild = 7.5e13/16384 / R_t
+    dxtild = dx / R_t
     if "label" in f.keys():
         label, = f["label"][()]
         label = label.decode('utf-8').strip(" '")
     else:
         label = str(i)
+    Eeng = (2047.49866274 / teng) * 1.97392088e52
+    Etild = 1e51 / (Eeng)
     
     ##################################################    
     # Replace spurious points in r_inner and r_outer #
     ##################################################
     
-    mask = r_outer > 7.4e13
+    mask = r_outer > (rmax - dx/2.)
     for j, row in enumerate(mask):
         if not row.any():
             continue
         r_outer[j, row] = np.interp(th[row], th[~row], r_outer[j, ~row])
 
-    mask = r_inner <= (7.5e13/16384*2. * 1.01)
+    mask = r_inner <= (dx/2.)
     for j, row in enumerate(mask):
         if not row.any():
             continue
@@ -83,30 +122,35 @@ for i, fname in enumerate(args.datafiles):
     else:
         color = colors[i]
     
-    #############################    
-    # Breakout time measurement #
-    #############################
+    #####################################    
+    # Breakout time estimate with angle #
+    #####################################
     
-    defl_par = (r_inner.T / np.median(r_inner, axis=1)).T
-    # defl_vel = np.empty_like(defl_par)
-    # defl_vel[5:] = (defl_par[5:] - defl_par[:-5]) / (5 * dttild)
-    # defl_vel[:5] = 0.0
-    defl_mask = (defl_par > args.breakout_thresh).astype(np.float64)
-    defl_mask = uniform_filter1d(defl_mask, size=args.window_size, axis=0, mode="constant", origin=-1)
-    idx_bo = np.argmax(defl_mask >= args.breakout_frac, axis=0)
-    idx_bo[idx_bo == 0] = -1
-    shift_bo = 0.5 * (args.window_size - np.ceil(args.window_size * args.breakout_frac))
-    t_bo = t[idx_bo]
-    ttild_bo = ttild[idx_bo]
+    if args.breakout_summary:
+        
+        defl_par = (r_inner.T / np.median(r_inner, axis=1)).T
+        # defl_vel = np.empty_like(defl_par)
+        # defl_vel[1:-1] = (defl_par[2:] - defl_par[:-2]) / (2 * dttild)
+        # defl_vel[1] = defl_vel[-1] = 0.0
+        defl_mask = (defl_par > args.breakout_thresh[i]).astype(np.float64)
+        defl_mask = uniform_filter1d(defl_mask, size=args.window_size[i], axis=0, mode="constant",
+                origin=-1)
+        idx_bo = np.argmax(defl_mask >= args.breakout_frac[i], axis=0)
+        idx_bo[idx_bo == 0] = -1
+        shift_bo = 0.5 * (args.window_size[i] - np.ceil(args.window_size[i] * args.breakout_frac[i]))
+        t_bo = t[idx_bo]
+        ttild_bo = ttild[idx_bo]
             
-    #################  
-    # Power-law fit #
-    #################
+    ##########################################
+    # Power-law fit to median shell position #
+    ##########################################
     
     if args.tmax_fit:
-        if args.tmax_fit < 0.0:
-            args.tmax_fit = ttild_bo.min()
-        mask = ttild < args.tmax_fit
+        if args.tmax_fit[i] < 0.0:
+            tmax_fit = ttild_bo.min()
+        else:
+            tmax_fit = args.tmax_fit[i]
+        mask = ttild < tmax_fit
         ttild_m = ttild[mask]
         r_peak_tild_m = r_peak_tild[mask]
     else:
@@ -115,10 +159,28 @@ for i, fname in enumerate(args.datafiles):
         
     nth = r_peak_tild_m.shape[1]
     sigma = np.sqrt(np.pi * (2*nth + 1) / (4*nth)) * r_peak_tild_m.std(axis=1)
-    fit_par, fit_cov = curve_fit(analytical_func, ttild_m, np.median(r_peak_tild_m, axis=1),
+    fit_par, fit_cov = curve_fit(analytical_pos, ttild_m, np.median(r_peak_tild_m, axis=1),
             sigma=sigma)
     fit_std = np.sqrt(np.diag(fit_cov))
-    fit_data = analytical_func(ttild, *fit_par)
+    A, alpha = fit_par
+    
+    #####################################  
+    # Power-law fit to shell deflection #
+    #####################################
+    
+    if args.tmax_tbo:
+        mask = ttild < args.tmax_tbo[i]
+        ttild_m = ttild[mask]
+        r_peak_tild_m = r_peak_tild[mask]
+    else:
+        ttild_m = ttild
+        r_peak_tild_m = r_peak_tild
+        
+    fit_delt_par, fit_delt_cov = curve_fit(analytical_pos, ttild[mask],
+            r_peak_tild[mask].max(axis=1) - np.median(r_peak_tild[mask], axis=1))
+    fit_delt_std = np.sqrt(np.diag(fit_delt_cov))
+    Adelt, alphadelt = fit_delt_par
+    t_first_bo = ((A * alpha) / (Adelt * alphadelt) * 0.6)**(1. / (alphadelt - alpha))
     
     ################## 
     # Console output #
@@ -126,65 +188,58 @@ for i, fname in enumerate(args.datafiles):
     
     print(f"{label}:")
     print(f"A = {fit_par[0]} ± {fit_std[0]}; α = {fit_par[1]} ± {fit_std[1]}")
-    t_bo_idx = np.argsort(t_bo)
-    for n_j, j in enumerate(t_bo_idx):
-        eq_ang = np.abs(np.pi/2 - th[j])*180./np.pi
-        print(f"({1.3*ttild_bo[j]:.2f}, {eq_ang:.1f})", end=" ")
-        if (n_j+1) % 6 == 0:
-            print()
-    print()
+    print(f"t_first_bo = {t_first_bo}")
+    if args.breakout_summary:
+        t_bo_idx = np.argsort(t_bo)
+        for n_j, j in enumerate(t_bo_idx):
+            eq_ang = np.abs(np.pi/2 - th[j])*180./np.pi
+            print(f"({ttild_bo[j]:.2f}, {eq_ang:.1f})", end=" ")
+            if (n_j+1) % 6 == 0:
+                print()
+    print('\n')
     
     #########
     # Plots #
     #########
     
     plt.figure(0)
-    if args.tmax_tbo:
-        mask = ttild_bo < args.tmax_tbo
-        plt.plot(th[mask], ttild_bo[mask], label=label, color=color)
-    else:
-        plt.plot(th, ttild_bo, label=label, color=color)
     
     if args.tmax_xs:
-        mask = ttild < args.tmax_xs
-        ttild_m = ttild[mask]
-        r_peak_tild_m = r_peak_tild[mask]
-        fit_data_m = fit_data[mask]
+        mask = ttild < args.tmax_xs[i]
+        ttild_m = ttild[mask][::args.plot_int[i]]
+        r_peak_tild_m = r_peak_tild[mask][::args.plot_int[i]]
     else:
-        ttild_m = ttild
-        r_peak_tild_m = r_peak_tild
-        fit_data_m = fit_data
-    
-    plt.figure(1)
-    # yerr = np.sqrt(np.pi * (2*nth + 1) / (4*nth)) * drtild / np.sqrt(nth)
+        ttild_m = ttild[::plot_int]
+        r_peak_tild_m = r_peak_tild[::plot_int]
+        
+    # yerr = np.sqrt(np.pi * (2*nth + 1) / (4*nth)) * dxtild / np.sqrt(nth)
     rp_med = np.median(r_peak_tild_m, axis=1)
     rp_min = r_peak_tild_m.min(axis=1)
     rp_max = r_peak_tild_m.max(axis=1)
     yerr = [rp_med - rp_min, rp_max - rp_med]
-    plt.errorbar(ttild_m, rp_med, yerr=yerr, fmt=".", label=label,
+    plt.errorbar(ttild_m / Etild, rp_med / Etild, yerr=yerr/Etild, fmt=".", label=label,
             markersize=8, capsize=2, color=color)
-    # plt.errorbar(ttild_m, r_peak_tild_m.min(axis=1), yerr=drtild, fmt="^",
+    # plt.errorbar(ttild_m * Etild, r_peak_tild_m.min(axis=1), yerr=dxtild, fmt="^",
     #          markersize=4, capsize=2, color=color)
-    # plt.errorbar(ttild_m, r_peak_tild_m.max(axis=1), yerr=drtild, fmt="v",
+    # plt.errorbar(ttild_m * Etild, r_peak_tild_m.max(axis=1), yerr=dxtild, fmt="v",
     #          markersize=4, capsize=2, color=color)
     
-    plt.plot(ttild_m, fit_data_m, color=color, linestyle="--")
-            
-    #plt.errorbar(t, r_inner.mean(axis=1), yerr=r_peak.std(axis=1), xerr=dt, fmt="^",
-    #        markersize=8, capsize=2, color=color)
-    #plt.errorbar(t, r_outer.mean(axis=1), yerr=r_peak.std(axis=1), xerr=dt, fmt="v",
-    #        markersize=8, capsize=2, color=color)
+    plt.plot(ttild_m / Etild, analytical_pos(ttild_m, *fit_par) / Etild, color=color, linestyle="--")
+    
+    plt.figure(1)
+    plt.plot(ttild_m / Etild, (rp_max - rp_med) / Etild, color=color, label=label)
+    plt.plot(ttild_m / Etild, analytical_pos(ttild_m, *fit_delt_par) / Etild, color=color, linestyle="--")
     
     f.close()
 
 plt.figure(0)
-plt.xlabel(r'$\theta$ [rad]')
-plt.ylabel(r'$\tilde{t}_{\mathrm{bo}}$')
-plt.legend()
-plt.gcf().savefig("t_bo_vs_ang.png")
-
-plt.figure(1)
-plt.xlabel(r'$\tilde{t}$')
-plt.ylabel(r'$\tilde{x}_{s}$')
+plt.xlabel(r'$\tilde{t}~/~\tilde{E}_{\mathrm{kin,ej}}$')
+plt.ylabel(r'$\tilde{x}_{s}~/~\tilde{E}_{\mathrm{kin,ej}}$')
 plt.legend()
 plt.gcf().savefig("shock_pos_vs_time.png")
+
+plt.figure(1)
+plt.xlabel(r'$\tilde{t}~/~\tilde{E}_{\mathrm{kin,ej}}$')
+plt.ylabel(r'$\tilde{delta}~/~\tilde{E}_{\mathrm{kin,ej}}$')
+plt.legend()
+plt.gcf().savefig("shell_defl_vs_time.png")
