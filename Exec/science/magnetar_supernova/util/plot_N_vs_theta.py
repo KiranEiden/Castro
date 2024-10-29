df88f31bbeeb110e8387afdb9a9fbdd13bb8f239
Kiran Eiden <keiden@eidensystems.com>
2024-10-29 14:44:53 -0700

    Part III of previous two commits

--- Exec/science/magnetar_supernova/util/plot_N_vs_theta.py
+++ Exec/science/magnetar_supernova/util/plot_N_vs_theta.py
@@ -26,7 +26,7 @@ for i, fname in enumerate(args.datafiles):
                 t = float(line.split(':')[1].strip())
     
     if args.labels is None:
-        label = f"t = {t/args.teng:.1f}" + r"$t_{\mathrm{eng}}$"
+        label = f"t = {t/args.teng:.2f}" + r"$t_{\mathrm{eng}}$"
     else:
         label = args.labels[i]
     plt.plot(ang*180/np.pi, N * (0.1 + t/args.teng)**2, label=label)
