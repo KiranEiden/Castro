8073ddd6f7011ab8bb0c2928373c75f0f72a31db
Kiran Eiden <keiden@eidensystems.com>
2024-10-29 14:26:50 -0700

    Switch to scaling off of t_eng

--- Exec/science/magnetar_supernova/util/plot_N_vs_theta.py
+++ Exec/science/magnetar_supernova/util/plot_N_vs_theta.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser()
 parser.add_argument('datafiles', nargs='+')
-parser.add_argument('--t0', type=float)
+parser.add_argument('--teng', type=float, default=2047.49826674)
 parser.add_argument('--labels', nargs='*')
 args = parser.parse_args()
 
@@ -29,10 +29,10 @@ for i, fname in enumerate(args.datafiles):
         label = f"t + t0 = {t:.1f}"
     else:
         label = args.labels[i]
-    plt.plot(ang*180/np.pi, N * (1. + t/args.t0)**2, label=label)
+    plt.plot(ang*180/np.pi, N * (0.1 + t/args.teng)**2, label=label)
 
 plt.xlabel(r'$\theta~(^{\circ})$')
-plt.ylabel(r'$N (1 + t/t_0)^2~(\mathrm{cm^{-2}})$')
+plt.ylabel(r'$N (\tilde{t}_0 + \tilde{t})^2~(\mathrm{cm^{-2}})$')
 plt.yscale("log")
 plt.legend()
 plt.gcf().savefig("N_vs_theta.png", dpi=480)
