c26cc37b62843c6a6e6dff91e1411ca669d25f1c
Kiran Eiden <kiran_eiden@berkeley.edu>
2024-10-31 22:53:48 -0700

    Add support for only plotting column density variation in first quadrant

--- Exec/science/magnetar_supernova/util/plot_N_vs_theta.py
+++ Exec/science/magnetar_supernova/util/plot_N_vs_theta.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser()
 parser.add_argument('datafiles', nargs='+')
 parser.add_argument('--teng', type=float, default=2047.49826674)
+parser.add_argument('--plot_half', action='store_true')
 parser.add_argument('--labels', nargs='*')
 args = parser.parse_args()
 
@@ -29,7 +30,16 @@ for i, fname in enumerate(args.datafiles):
         label = f"t = {t/args.teng:.2f}" + r"$t_{\mathrm{eng}}$"
     else:
         label = args.labels[i]
-    plt.plot(ang*180/np.pi, N * (0.1 + t/args.teng)**2, label=label, linewidth=1)
+
+    
+    if args.plot_half:
+        mask = ang <= np.pi/2.
+        th_plot = ang[mask]
+        N_plot = N[mask]
+    else:
+        th_plot = ang
+        N_plot = N
+    plt.plot(th_plot*180./np.pi, N_plot * (0.1 + t/args.teng)**2, label=label, linewidth=1)
 
 plt.xlabel(r'$\theta~(^{\circ})$')
 plt.ylabel(r'$N (\tilde{t}_0 + \tilde{t})^2~(\mathrm{cm^{-2}})$')
