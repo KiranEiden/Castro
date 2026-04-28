719268d0edaef19d2e67568bba2ed942362b79a0
Kiran Eiden <keiden@eidensystems.com>
2026-04-27 17:12:35 -0700

    Add code to fill underdense regions near vertical axis

--- Exec/science/aic_engine/aic_model_data.cpp
+++ Exec/science/aic_engine/aic_model_data.cpp
@@ -12,4 +12,8 @@ namespace aic_model_data
     AMREX_GPU_MANAGED Array1D<Real, 0, NR_MODEL-1> model_r_out;
     AMREX_GPU_MANAGED Array1D<Real, 0, NZ_MODEL-1> model_z_out;
     AMREX_GPU_MANAGED Array3D<Real, 0, nvars_model-1, 0, NR_MODEL-1, 0, NZ_MODEL-1> model_data;
+    
+    AMREX_GPU_MANAGED Array1D<int, 0, 1> jfill;
+    AMREX_GPU_MANAGED Array1D<int, 0, NZ_MODEL-1> imax_fill;
+    AMREX_GPU_MANAGED Array1D<Real, 0, 1> fill_vals;
 }
